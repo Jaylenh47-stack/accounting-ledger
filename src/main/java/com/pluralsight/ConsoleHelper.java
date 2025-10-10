c180e566a2b062e7685adaca85ca8db015aae317
Jaylenh47-stack <jaylenh47@gmail.com>
2025-10-10 09:26:01 -0400

    initial files and some comments added

---
+++ src/main/java/com/pluralsight/ConsoleHelper.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class ConsoleHelper {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int promptForInt(String prompt) {
+        int input = 0;
+        boolean isInvalid = false;
+
+        do {
+            try {
+                System.out.println(prompt + ": ");
+                input = scanner.nextInt();
+                scanner.nextLine();
+                isInvalid = true;
+            } catch (Exception ex) {
+                scanner.nextLine();
+                System.out.println("Invalid entry, please enter a whole number");
+            }
+
+        }
+        while (!isInvalid);
+
+        return input;
+    }
+
+    public static String promptForString(String prompt) {
+
+        System.out.println(prompt + ": ");
+        return scanner.nextLine();
+    }
+
+    public static float promptForFloat(String prompt){
+        float input = 0f;
+        boolean isInvalid = false;
+
+        do {
+            try {
+                System.out.println(prompt + ": ");
+                input = scanner.nextFloat();
+                scanner.nextLine();
+                isInvalid = true;
+            } catch (Exception ex) {
+                scanner.nextLine();
+                System.out.println("Invalid entry, please enter a number");
+            }
+
+        }
+        while (!isInvalid);
+
+        return input;
+    }
+
+
+}
---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+        //Home screen -loops until user exits
+
+
+
+        //D) add deposit - prompt and save to csv file, show balance after
+
+
+        //P) make payment - prompt for the debit information, save to csv file, show balance after
+
+
+        //L) Ledger - display the ledger screen
+
+    }
+}
