f76fa0439bead1947d4dbf901bcabd1dbcfcfc30
shewit <hailu.shewit47@gmail.com>
2026-01-16 10:33:07 -0500

    added home screen class

---
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class HomeScreen {
+
+    public static void main(String[] args) {
+
+        System.out.println("""
+                Welcome to your accounting ledger! What would u like to do today?
+                """);
+    }
+
+}
